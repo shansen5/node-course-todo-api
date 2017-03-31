754b2af622c308131113f6f319ae7332f5802efb
shansen5 <steven.e.hansen@gmail.com>
2017-03-31 14:56:09 -0700

    Added update example.

---
+++ playground/mongodb-update.js
@@ -0,0 +1,23 @@
+// const MongoClient = require( 'mongodb' ).MongoClient;
+const {MongoClient, ObjectID} = require( 'mongodb' );
+
+MongoClient.connect( 'mongodb://localhost:27017/TodoApp', (err, db) => {
+  if ( err ) {
+    return console.log( 'Unable to connection to mongodb server' );
+  }
+  console.log( 'Connected to mongodb server' );
+
+  db.collection( 'Users' ).updateMany( {
+    name: /^Fr/
+  }, {
+    $inc: {
+      age: 6
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log( result );
+  });
+
+  // db.close();
+});
