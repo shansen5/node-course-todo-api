3d78fb9927961ccd59dc9b6b49f1a704e60662b1
shansen5 <steven.e.hansen@gmail.com>
2017-04-06 12:56:37 -0700

    Adding tests.

--- server/server.js
+++ server/server.js
@@ -25,3 +25,5 @@ app.post( '/todos', (request, response) => {
 app.listen( 3000, () => {
   console.log( 'Started on port 3000' );
 });
+
+module.exports = {app};
---
+++ start-mongo.sh
@@ -0,0 +1,2 @@
+/home/steve/workspace/mongo/bin/mongod --dbpath ~/workspace/mongo-data/ &
+
